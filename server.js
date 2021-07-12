ee3d5ef6dc8f11f22bf478d60050a50ac7830fc8
Glitch (nosy-scarce-cousin) <none>
2021-07-12 22:46:47 +0000

    ❤️🍀 Checkpoint ./server.js:33002783/1101

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-var multer = require("multer")
+
 
 var app = express();
 
@@ -12,27 +12,25 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-/* User Story #1
-You can submit a form that includes a file upload. */
-app.post("")
-/* User Story #2
-The form file input field has the name attribute set to upfile. */
-
-/* User Story #3
-When you submit a file, you receive the file name, type, and size in bytes within the JSON response. */
-
-
-
-
-
-
-
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
 
-let multer = requir
+var multer = require("multer")
+
+app.post("/api/fileanalyse", multer().single("upfile"), (req, res) => {
+  let resObject = {}
+  resObject['name'] = req.file.originalname
+  resObject['type'] = req.file.mimetype
+  resObject['size'] = req.file.size
+  
+  
+  console.log(req.file)
+  res.json({
+    name: file.name,
+    type: file.type,
+    size: file.size
+  })
+})
+
